44884f54766ed3372d403bc18526cc6429c57388
fritzpal <henri@staudenrausch.de>
2024-12-08 20:30:05 +0100

    added zoom and movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public float zoomSpeed = 0.2f;
+    public int maxZoom = 10;
+    public int minZoom = 1;
+    public Camera mainCamera;
+    public float movementSpeed = 0.5f;
+    private float zoomLevel = 5;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        mainCamera.orthographicSize = zoomLevel;
+    }
+
+    void Update()
+    {
+        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, zoomLevel, Time.deltaTime * 10);
+
+        Vector3 move = new Vector3(moveDirection.x, moveDirection.y, 0);
+        transform.position += move * movementSpeed * Time.deltaTime * zoomLevel;
+    }
+
+    public void HandleScrollWheel(InputAction.CallbackContext context)
+    {
+        Vector2 scrollValue = context.ReadValue<Vector2>();
+        zoomLevel -= (int)scrollValue.y * zoomSpeed * zoomLevel;
+        zoomLevel = Mathf.Clamp(zoomLevel, minZoom, maxZoom);
+    }
+
+    public void HandleMove(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+}
