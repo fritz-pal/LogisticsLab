03396e41fad9468bf6488ba8d7f9cb091bce0dd0
fritzpal <henri@staudenrausch.de>
2024-12-09 18:27:28 +0100

    experimental implementation of underlying structure

--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -1,24 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Track : MonoBehaviour
+public class Track
 {
-    public bool hasSignal = false;
-    public Direction direction;
+    private (Node, Node) nodes;
+    private List<Track> crossedTracks = new List<Track>();
 
-    public Track(Direction direction)
+    public Track(Vector2Int start, Vector2Int end, Direction direction)
     {
-        this.direction = direction;
+        // calculate end direction
+        // calculate crossings
+        // make splines
+        Direction endDirection = direction;
+        nodes = (new(start, direction), new(end, endDirection));
+        nodes.Item1.SetSibling(nodes.Item2);
+        nodes.Item2.SetSibling(nodes.Item1);
+        GridManager.Instance.CreateNodeGroup(start, direction, new Node[] { nodes.Item1 });
+        GridManager.Instance.CreateNodeGroup(end, endDirection, new Node[] { nodes.Item2 });
     }
-}
 
-public enum Direction
-{
-    NORTH,
-    NORTHWEST,
-    WEST,
-    SOUTHWEST,
-    SOUTH,
-    SOUTHEAST,
-    EAST,
-    NORTHEAST
+    public void DebugTrack(){
+        Debug.Log("Track from " + nodes.Item1.position + " to " + nodes.Item2.position);
+    }
+
+    public void DrawTrack(){
+        Debug.DrawLine(new Vector3(nodes.Item1.position.x, nodes.Item1.position.y, 0), new Vector3(nodes.Item2.position.x, nodes.Item2.position.y, 0), Color.red);
+    }
 }
