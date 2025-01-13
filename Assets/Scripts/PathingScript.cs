3eb45431cd2628697f91e90dbecf639643b93662
Philipp Mayer <phmayer@stud.hs-heilbronn.de>
2025-01-13 09:15:25 +0100

    attempted dijkstra

--- Assets/Scripts/PathingScript.cs
+++ Assets/Scripts/PathingScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PathingScript : MonoBehaviour
@@ -14,11 +15,22 @@ public class PathingScript : MonoBehaviour
         
     }
 
-    public void GetPath(NodeGroup startNodeGroup, NodeGroup endNodeGroup)
+    /*
+     * returns a list of positions which then can be used to create a spline
+     */
+    public List<Vector2Int> GetPath(NodeGroup startNodeGroup, NodeGroup endNodeGroup)
     {
+        //nodegroups are dijkstra-nodes, we iterate through our train network
+        //costs between nodes are just number of tracks, or for the start its just always 1 between nodegroups,
+        //since the distances are somewhat similar
         
         
+        //look for unvisited node (if none can be found end it)
+        //if unvisited node found, calculate 
         
+        
+
+        return null;
     }
     
 }
---
+++ Assets/Scripts/dijkstra/Edge.cs
@@ -0,0 +1,15 @@
+﻿namespace dijkstra
+{
+    public class Edge
+    {
+        public string To { get; }
+        public int Cost { get; }
+
+        public Edge(string to, int cost)
+        {
+            To = to;
+            Cost = cost;
+        }
+    }
+
+}
