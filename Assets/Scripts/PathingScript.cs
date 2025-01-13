486b9c39be2b0d385a0bd50c67f11561c077475e
Philipp Mayer <phmayer@stud.hs-heilbronn.de>
2025-01-13 03:15:52 +0100

    add pathing stuff

---
+++ Assets/Scripts/PathingScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PathingScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetPath(NodeGroup startNodeGroup, NodeGroup endNodeGroup)
+    {
+        
+        
+        
+    }
+    
+}
