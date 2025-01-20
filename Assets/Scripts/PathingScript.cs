d3c5ffb75ca0a746fb7f0876513967deb1a0dabc
fritzpal <henri@staudenrausch.de>
2025-01-20 14:58:09 +0100

    pathing doesnt work

--- Assets/Scripts/PathingScript.cs
+++ Assets/Scripts/PathingScript.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEngine;
 
 public class PathingScript
 {
@@ -6,7 +7,7 @@ public class PathingScript
     private int lowestCost;
     private Node startNode;
     private List<Node> destinationNodes;
-    private Direction destinationDirection; 
+    private Direction destinationDirection;
 
     /*
      * returns a list of nodes, that represent the shortest path from a given start node group to a destination node group
@@ -21,20 +22,24 @@ public class PathingScript
             startNode = null;
             destinationNodes = endNodeGroup.GetNodes();
             destinationDirection = Direction.NONE;
-        
-            //get node with witch we start, from node group:
+
+            //get node with which we start, from node group:
             foreach (Node n in startNodeGroup.GetNodes())
             {
-                if(n.GetTransitions()[0].direction == trainDirection)
+                if ((int)n.direction == ((int)trainDirection + 4) % 8)
+                {
                     startNode = n;
+                    break;
+                }
             }
-        
+
             //call our recursive method
             GoToNextNode(startNode, 0);
-        
+
             //search dict for all destination nodes and pick the one with the lowest cost 
             int lowestDestinationCost = int.MaxValue;
             Node finalDestinationNode = null;
+            Debug.Assert(destinationNodes.Count > 0, "No destination nodes found");
             foreach (Node n in destinationNodes)
             {
                 if (visitedNodes.ContainsKey(n))
@@ -44,6 +49,7 @@ public class PathingScript
                         finalDestinationNode = n;
                     }
             }
+            Debug.Assert(finalDestinationNode != null, "No final destination node found");
             //once recursive method finished, just go from the destination node back to the start using the previous node entry,
             //and this will then create our path
             List<Node> path = new List<Node>();
@@ -65,7 +71,7 @@ public class PathingScript
         foreach (Node n in pathNodes)
         {
             if (path.Count == 0 || (path.Count > 0 && path[0] != n.GetNodeGroup()))
-            path.Insert(0, n.GetNodeGroup());
+                path.Insert(0, n.GetNodeGroup());
         }
         return path;
     }
@@ -77,6 +83,8 @@ public class PathingScript
 
     private void GoToNextNode(Node node, int cost)
     {
+        if (node == null)
+            return;
         //check if done/end etc.
         if (cost < lowestCost)
             lowestCost = cost;
@@ -86,11 +94,12 @@ public class PathingScript
         foreach (Node n in destinationNodes)
             if (n == node)
                 return;
-        
+
         foreach (Node transitionNode in node.GetTransitions())
-        {   
+        {
             Node nextNode = transitionNode.GetSibling();
-            
+            Debug.Assert(nextNode != null, "Sibling of node is null");
+
             //look for the next node in our visited nodes list:
             if (visitedNodes.ContainsKey(nextNode))
             {
@@ -116,5 +125,4 @@ public class PathingScript
             }
         }
     }
-    
 }
